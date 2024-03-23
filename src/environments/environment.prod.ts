0316dc10970f42003d9dc5fc0fb7a86dee66d680
diego <diego.arias@gmail.com>
2024-03-23 20:49:29 -0300

    add change logo functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
   SERVER_API: '/assets/mockData/movies.json',
-  DEFAULT_LOGO: '/assets/images/logo.png'
+  DEFAULT_LOGO: '/assets/images/logo.png',
+  APP_LOGOS: [
+     '/assets/images/logo.png',
+     '/assets/images/logo2.jpg',
+     '/assets/images/logo3.jpg',
+     '/assets/images/logo4.jpg',
+     '/assets/images/logo5.jpg'
+  ]
 };
